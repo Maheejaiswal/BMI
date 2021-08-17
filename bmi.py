4aee0bd325e8707a9770740b5738517b105e645d
greeneyedcoder <mahee.jaiswal@cumminscollege.edu.in>
2021-08-17 11:46:06 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -7,8 +7,8 @@ height_as_float = float(height)
 
 # Using the exponent operator **
 bmi = weight_as_int / height_as_float ** 2
-// # or using multiplication and PEMDAS
-//bmi = weight_as_int / (height_as_float * height_as_float)
+# or using multiplication and PEMDAS
+#bmi = weight_as_int / (height_as_float * height_as_float)
 
 bmi_as_int = int(bmi)
 
